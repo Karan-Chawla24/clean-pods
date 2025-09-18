102014413a29673bb635956182191bb7eb64cead
KaranChawla <chawlakaran663@gmail.com>
2025-09-18 22:53:14 +0530

    added the policies for phonepe

--- next.config.js
+++ next.config.js
@@ -7,12 +7,13 @@ const nextConfig = {
 
     const ContentSecurityPolicy = [
       "default-src 'self'",
-      `script-src 'self' ${isProd ? "" : "'unsafe-eval'"} https://js.stripe.com https://*.clerk.accounts.dev https://*.clerk.com https://*.phonepe.com`,
+      `script-src 'self' ${isProd ? "" : "'unsafe-eval'"} https://js.stripe.com https://*.clerk.accounts.dev https://*.clerk.com https://*.phonepe.com https://mercury.phonepe.com https://mercury-t2.phonepe.com https://mercurystatic.phonepe.com https://linchpin.phonepe.com https://www.google-analytics.com https://dgq88cldibal5.cloudfront.net`,
       "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
       "img-src 'self' data: https: blob:",
       "font-src 'self' https://fonts.gstatic.com",
-      "connect-src 'self' https://hooks.slack.com https://*.clerk.accounts.dev https://*.clerk.com https://api.clerk.com https://api.phonepe.com https://api-preprod.phonepe.com",
-      "frame-src 'self' https://*.clerk.accounts.dev https://*.clerk.com https://*.phonepe.com",
+      "connect-src 'self' https://hooks.slack.com https://*.clerk.accounts.dev https://*.clerk.com https://api.clerk.com https://api.phonepe.com https://api-preprod.phonepe.com https://mercury.phonepe.com https://mercury-t2.phonepe.com https://sentry.phonepe.com https://mercurystatic.phonepe.com https://linchpin.phonepe.com https://www.google-analytics.com",
+      "frame-src 'self' https://*.clerk.accounts.dev https://*.clerk.com https://*.phonepe.com https://mercury.phonepe.com https://mercury-t2.phonepe.com",
+      "worker-src 'self' blob: https://mercury.phonepe.com https://mercury-t2.phonepe.com",
       "object-src 'none'",
       "base-uri 'self'",
       "form-action 'self'",
